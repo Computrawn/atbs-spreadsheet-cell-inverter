19e67b7ba734240982af0bddcce4ee0bf0fbc3a8
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-17 13:37:55 -0500

    Finish functionality.

--- spreadsheetCellInverter.py
+++ spreadsheetCellInverter.py
@@ -21,8 +21,14 @@ def cell_inverter(document):
             column_list.append(sheet[f"{column_letter}{row}"].value)
         xy_list.append(column_list)
 
-    wb.save(f"{document}_inverted.xlsx")
+    inv_wb = openpyxl.Workbook()
+    inv_sheet = inv_wb.active
+    for y in range(1, sheet.max_column + 1):
+        for x in range(1, sheet.max_row + 1):
+            letter = get_column_letter(x)
+            inv_sheet[f"{letter}{y}"] = xy_list[y - 1][x - 1]
+
+    inv_wb.save(f"{document}_inverted.xlsx")
 
 
 cell_inverter(file_name)
-print(xy_list)
