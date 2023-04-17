1cd47931cdf4c1dfa873dd17d151aa1aec10bca2
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-17 12:42:39 -0500

    Make loop for list of lists data structure.

--- spreadsheetCellInverter.py
+++ spreadsheetCellInverter.py
@@ -3,16 +3,26 @@
 # For more information, see project_details.txt.
 
 import openpyxl
+from openpyxl.utils import get_column_letter
 
 file_name = input("Please enter filename here: ")
+xy_list = []
 
 
 def cell_inverter(document):
     """Opens xlsx file, inverts its cells and saves the inverted file."""
     wb = openpyxl.load_workbook(f"{document}.xlsx")
     sheet = wb.active
-    # TODO: write nested loops to invert document cells.
+
+    for column in range(1, sheet.max_column + 1):
+        column_letter = get_column_letter(column)
+        column_list = []
+        for row in range(1, sheet.max_row + 1):
+            column_list.append(sheet[f"{column_letter}{row}"].value)
+        xy_list.append(column_list)
+
     wb.save(f"{document}_inverted.xlsx")
 
 
 cell_inverter(file_name)
+print(xy_list)
